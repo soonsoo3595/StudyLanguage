ec80ca682f1c3e1d87b8680c78282ed74ea711c6
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2024-02-03 00:18:20 +0900

    캐스팅 공부

--- Study/Casting.cpp
+++ Study/Casting.cpp
@@ -12,6 +12,8 @@ public:
 		cout << "a = " << a << endl;
 		cout << "b = " << b << endl;
 	}
+
+	virtual ~Base() = default;
 protected:
 	int a = 10;
 	double b = 20.5;
@@ -28,6 +30,8 @@ public:
 		cout << "c = " << c << endl;
 		cout << "d = " << d << endl;
 	}
+
+	virtual ~Derived() = default;
 protected:
 	int c = 30;
 	double d = 40.5;
@@ -59,10 +63,27 @@ int main()
 	// cout << "p3 = " << *p3 << endl;
 	// cout << "p4 = " << *p4 << endl;
 
+	// Base* b;
+	// Derived* d = new Derived();
+	// 
+	// b = static_cast<Base*>(d);	// 업캐스팅
+	// b->Print();
+
+	// Base* b = new Base();
+	// Derived* d;
+	// 
+	// d = static_cast<Derived*>(b);	// 다운캐스팅
+	// d->Print();	// 쓰레기값
+
+	// const int cnum = 10;
+	// const int* ptrcnum = &cnum;
+	// int* cnum2 = const_cast<int*>(ptrcnum);
+
+	// double d = 3.14;
+	// char p = reinterpret_cast<char>(&d);
+
 	Base* b;
 	Derived* d = new Derived();
-
-	b = static_cast<Base*>(d);
-	b->Print();
-
+	b = d;
+	d = dynamic_cast<Derived*>(b);	// 클래스에 가상 함수가 없을 때는 컴파일 에러
 }
