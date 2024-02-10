cd36772e369272cc0213714551fe1f6bd52d9e96
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2024-02-11 02:42:25 +0900

    인라인 함수

---
+++ Study/inline.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+inline int sum(int a, int b)
+{
+	return a + b;
+}
+
+__inline int minus(int a, int b)
+{
+	return a - b;
+}
+
+__forceinline int multiply(int a, int b)
+{
+	return a * b;
+}
+
+int main()
+{
+	int num = 0;
+
+	num = sum(1, 2);
+
+	num = minus(2, 1);
+
+	num = multiply(2, 4);
+}
